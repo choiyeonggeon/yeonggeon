4683b24594dceecb2289dec9119e8cdb92f835b9
choiyeonggeon <aws8792@naver.com>
2025-04-24 12:37:43 +0900

    미완성

---
+++ poketmonAppp/poketmonAppp/UserContact.swift
@@ -0,0 +1,106 @@
+//
+//  Usercontact.swift
+//  poketmonAppp
+//
+//  Created by 최영건 on 4/23/25.
+//
+
+import UIKit
+import SnapKit
+
+class UserContact: UIViewController {
+
+
+    let userProfileImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.layer.borderWidth = 1
+        imageView.layer.cornerRadius = 80
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+
+    lazy var randomImageBtn: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("랜덤 이미지 생성", for: .normal)
+        button.addTarget(self, action: #selector(randomImageBtntapped), for: .touchUpInside)
+        return button
+    }()
+
+    let nameTextView: UITextView = {
+        let textView = UITextView()
+        textView.layer.borderWidth = 1
+        textView.layer.borderColor = UIColor.gray.cgColor
+        return textView
+    }()
+
+    let numTextView: UITextView = {
+        let textView = UITextView()
+        textView.layer.borderWidth = 1
+        textView.layer.borderColor = UIColor.gray.cgColor
+        return textView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+        
+        view.addSubview(userProfileImage)
+        view.addSubview(randomImageBtn)
+        view.addSubview(nameTextView)
+        view.addSubview(numTextView)
+
+
+        UserNaviBarSetup()
+        UserSetupConstraints()
+    }
+
+    //navigationBarSetup 설정
+    private func UserNaviBarSetup() {
+//        let navigationItem = UINavigationItem(title: "친구 목록")
+        let okBtn = UIBarButtonItem(title: "적용", style: .plain, target: self, action: #selector(btnTapped))
+        self.navigationItem.rightBarButtonItem = okBtn
+        self.title = "연락처 추가"
+    }
+    func UserSetupConstraints() {
+        //userProfileImage 제약조건
+        userProfileImage.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(40)
+            $0.centerX.equalToSuperview()
+            $0.width.height.equalTo(160)
+        }
+        //randomImageBtn 제약조건
+        randomImageBtn.snp.makeConstraints {
+            $0.top.equalTo(userProfileImage.snp.bottom).offset(20)
+            $0.centerX.equalToSuperview()
+        }
+        //nameTextView 제약조건
+        nameTextView.snp.makeConstraints {
+            $0.top.equalTo(randomImageBtn.snp.bottom).offset(30)
+            $0.leading.equalToSuperview().offset(20)
+            $0.trailing.equalToSuperview().offset(-20)
+            $0.height.equalTo(40)
+        }
+        //numTextView 제약조건
+        numTextView.snp.makeConstraints {
+            $0.top.equalTo(nameTextView.snp.bottom).offset(30)
+            $0.leading.equalToSuperview().offset(20)
+            $0.trailing.equalToSuperview().offset(-20)
+            $0.height.equalTo(40)
+        }
+    }
+
+    // addBtnTapped 메서드 생성
+    @objc func btnTapped() {
+
+    }
+
+    @objc func randomImageBtntapped(){
+        ramdomPokemonImage { [weak self] image in
+            DispatchQueue.main.async {
+                self?.userProfileImage.image = image
+            }
+        }
+    }
+}
---
+++ poketmonAppp/poketmonApppTests/poketmonApppTests.swift
@@ -0,0 +1,17 @@
+//
+//  poketmonApppTests.swift
+//  poketmonApppTests
+//
+//  Created by 최영건 on 4/23/25.
+//
+
+import Testing
+@testable import poketmonAppp
+
+struct poketmonApppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
